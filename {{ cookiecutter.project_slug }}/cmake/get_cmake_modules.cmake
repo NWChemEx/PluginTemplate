dc04bfe907f6e7ea093efe7ef3bc2a63f3cc0fc1
Ryan M. Richard <ryanmrichard1@gmail.com>
2024-11-17 22:06:50 -0600

    bump cmaize version

--- {{ cookiecutter.project_slug }}/cmake/get_cmake_modules.cmake
+++ {{ cookiecutter.project_slug }}/cmake/get_cmake_modules.cmake
@@ -16,7 +16,7 @@ macro(get_cmake_modules)
     )
     {% else %}
     if("${CMAIZE_VERSION}" STREQUAL "")
-        set(CMAIZE_VERSION v1.1.0 )
+        set(CMAIZE_VERSION v1.1.5 )
     endif()
 
     # Store whether we are building tests or not, then turn off the tests
